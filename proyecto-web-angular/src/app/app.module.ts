6d6e0a38c788c1990bf3368fdb71054b6ce2b7a3
RodrigoGM97 <rodrigogm971@gmail.com>
2019-06-19 21:40:33 -0500

    elimine navbar

--- proyecto-web-angular/src/app/app.module.ts
+++ proyecto-web-angular/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
 import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     LoginComponent
   ],
   imports: [
