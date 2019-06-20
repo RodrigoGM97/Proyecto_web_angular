372613de82b18fae02c733eba28bdba7487bda53
RodrigoGM97 <rodrigogm971@gmail.com>
2019-06-19 21:33:42 -0500

    setup

--- proyecto-web-angular/src/app/app.module.ts
+++ proyecto-web-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
