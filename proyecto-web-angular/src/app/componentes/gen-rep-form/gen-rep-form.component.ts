d4a2601a62ebc420f5f395cb9f1bcc299c548e18
RodrigoGM97 <rodrigogm971@gmail.com>
2019-06-20 21:09:52 -0500

    hola

---
+++ proyecto-web-angular/src/app/componentes/gen-rep-form/gen-rep-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-rep-form',
+  templateUrl: './gen-rep-form.component.html',
+  styleUrls: ['./gen-rep-form.component.scss']
+})
+export class GenRepFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
