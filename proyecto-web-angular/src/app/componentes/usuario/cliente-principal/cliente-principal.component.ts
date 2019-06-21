4865d4c79454681bdcbb81997ea6a19723238c5c
RodrigoGM97 <rodrigogm971@gmail.com>
2019-06-20 19:43:16 -0500

    cambie dropdowns de clientes

--- proyecto-web-angular/src/app/componentes/usuario/cliente-principal/cliente-principal.component.ts
+++ proyecto-web-angular/src/app/componentes/usuario/cliente-principal/cliente-principal.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-principal.component.scss']
 })
 export class ClientePrincipalComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
