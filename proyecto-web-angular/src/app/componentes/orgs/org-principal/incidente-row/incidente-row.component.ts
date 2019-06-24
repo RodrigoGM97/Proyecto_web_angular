896aa0d40006059f40742503f3e10c94d0889cdf
saulelabra <quiquelabra@gmail.com>
2019-06-23 23:08:28 -0500

    servicio organizacion y avance login

--- proyecto-web-angular/src/app/componentes/orgs/org-principal/incidente-row/incidente-row.component.ts
+++ proyecto-web-angular/src/app/componentes/orgs/org-principal/incidente-row/incidente-row.component.ts
@@ -14,4 +14,14 @@ export class IncidenteRowComponent implements OnInit {
   ngOnInit() {
   }
 
+  incidenteCompletado() {
+    this.incidente.estado = "completado";
+    console.log("Se marco incidente '" + this.incidente.comentario + "' como completado");
+  }
+
+  incidenteRechazado() {
+    this.incidente.estado = "rechazado";
+    console.log("Se marco incidente '" + this.incidente.comentario + "' como rechazado");
+  }
+
 }
