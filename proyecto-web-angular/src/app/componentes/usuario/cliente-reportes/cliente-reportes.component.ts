38e4ea716951fb2e4ecea5b9aad718d95fb5f979
ManuGuV <manologuadarramav@hotmail.com>
2019-07-03 20:29:06 -0500

    client pages

--- proyecto-web-angular/src/app/componentes/usuario/cliente-reportes/cliente-reportes.component.ts
+++ proyecto-web-angular/src/app/componentes/usuario/cliente-reportes/cliente-reportes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioReportesService } from "../../../services/usuario-reportes/usuario-reportes.service";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cliente-reportes',
@@ -10,18 +11,51 @@ import { UsuarioReportesService } from "../../../services/usuario-reportes/usuar
 export class ClienteReportesComponent implements OnInit {
 
   incidentes;
+  public activeLang = 'es';
 
   show:boolean = false;
   toggleCollapse() {
     this.show = !this.show
   }
 
-  constructor(UsuarioReportesService: UsuarioReportesService) { 
+  selectedOption: string;
+  selectedOption2: string;
+  selectedOption3: string;
+
+  options = [
+    { name: "11/06/2019", value: 1 },
+    { name: "12/06/2019", value: 2 },
+    { name: "13/06/2019", value: 3 }
+  ]
+  options2 = [
+    { name: "Álvaro Obregón", value: 1 },
+    { name: "Benito Juárez", value: 2 },
+    { name: "Cuajimalpa", value: 3 }
+  ]
+  options3 = [
+    { name: "Lomas de Chapultepec", value: 1 },
+    { name: "Lomas de Reforma", value: 2 },
+    { name: "Florida", value: 3 }
+  ]
+
+  print() {
+    console.log("My input: ", this.selectedOption);
+    console.log("My input: ", this.selectedOption2);
+    console.log("My input: ", this.selectedOption3);
+  }
+
+  constructor(UsuarioReportesService: UsuarioReportesService, private translate:TranslateService) { 
     this.incidentes = UsuarioReportesService.getReportes();
     console.log(this.incidentes);
+    this.translate.setDefaultLang(this.activeLang);
   }
 
   ngOnInit() {
+  }
+
+  public cambiarLenguaje(lang){
+    this.activeLang = lang;
+    this.translate.use(lang);
   }
 
 }
